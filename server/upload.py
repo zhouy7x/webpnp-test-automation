f53a3b7e36c3f267c694477b19e278e13a1eb956
Wanming Lin <wanming.lin@intel.com>
2020-06-22 13:06:34 +0800

    Fixed excel unremoved error

--- server/upload.py
+++ server/upload.py
@@ -20,11 +20,9 @@ def get_files_str():
         files_list.append(fstr)
 
     o = subprocess.check_call("curl %s -F project=%d http://webpnp.sh.intel.com/api/report/ -H 'Authorization: Token  %s' " % (' '.join(files_list), project, token[user]), shell=True)
-    print(o)
-    if o == 0:
-        print("backup files")
-        for src_file in src_list:
-            os.rename(src_file, os.path.join('/home/banana/PHP/bak', os.path.basename(src_file)))
+    print("backup files")
+    for src_file in src_list:
+        os.rename(src_file, os.path.join('/home/banana/PHP/bak', os.path.basename(src_file)))
 
 if __name__ == '__main__':
     get_files_str()
